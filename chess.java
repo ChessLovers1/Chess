e37594fe113f111ff185dcca5ad0454246580c05
Alex <145604478+AlexMcIvor05@users.noreply.github.com>
2024-04-22 22:56:11 -0400

    chess

---
+++ chess.java
@@ -0,0 +1,44 @@
+public class chess {
+    
+    public class piece{
+     string png;
+     int[] moves;
+     int color;
+
+        
+        piece(string png, int[] position, int[] moves,int  color){
+
+        }
+
+        //
+        //public updateMoves(){
+
+        //}
+        
+        /*
+        IsBlocked
+
+        pawn
+        board[position-1][position]
+        
+        
+
+
+
+         */
+
+    }
+
+    public class board{
+        int[] board;
+
+        
+
+
+    }
+    public static Void main(args string){
+
+
+
+    }
+}
