39bbae6901c245d125d1868df95c1a5962d9acc5
Beatle <123680498+DavidCoderror@users.noreply.github.com>
2024-04-29 23:01:05 -0400

    Update GUI.java

--- src/program/GUI.java
+++ src/program/GUI.java
@@ -1,32 +1,27 @@
 package program;
 
 import java.awt.Canvas;
-
 import java.awt.Color;
 import javax.swing.JFrame;
-import javax.imageio.ImageIO;
-import java.io.File;
 
 public class GUI extends JFrame {
-	
-	public Canvas canvas;
-		
+
     public GUI() {
-        super("Blank Page");
+    	super("Chess"); 
+    	
+    	//ChessBoard background
+        ChessBoardPanel chessBoardPanel = new ChessBoardPanel();
+        add(chessBoardPanel);
 
-        canvas = new Canvas();
-
-        // Set canvas background color to white
-        canvas.setBackground(Color.WHITE);
-
-        add(canvas);
-        setSize(800, 600); // Set your desired size
+ 
+        setSize(830, 850); // Set your desired size
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the application when the window is closed
         setLocationRelativeTo(null); // Center the window on the screen
         setVisible(true); // Make the window visible
     }
 
     public static void main(String[] args) {
-        
+        new GUI();
+        System.out.println("Hello World");
     }
 }
