43187acf8bd442b608a1de23ff5844a237497dd7
Alex McIvor <alex.mcivor28@gmail.com>
2024-08-15 22:09:35 -0400

    added bishop method

--- src/program/Chess.java
+++ src/program/Chess.java
@@ -32,6 +32,7 @@ public class Chess {
 	//}
 	
     public static boolean validMove(Board board,int yIni,int xIni,int y,int x ){
+    	
         boolean result = false;
     	Piece piece = board.getPiece(yIni, xIni);
     	
@@ -45,26 +46,33 @@ public class Chess {
         //1. Check if There is a piece on the final square
         //2.Check if the pieces are different colors
     	if(!validCapture(board,yIni,xIni,y,x )) {
+    		System.out.println("not valid colors");
     		return false;
     	}
 
         //check if valid move for that specific piece
         if(piece.getClass() == new Queen(null).getClass()){
-            result = false;
+        	System.out.println("Queen");
+        	result = false;
         }
         else if(piece.getClass() == new Rook(null).getClass()){
+        	System.out.println("Rook");
         	result = rookMove(board,yIni,xIni,y,x );
         }
         else if(piece.getClass() == new Pawn(null).getClass()){
+        	System.out.println("Pawn");
         	result = false;
         }
         else if(piece.getClass() == new Bishop(null).getClass()){
-        	result = false;
+        	System.out.println("bishop");
+        	result = bishopMove(board,yIni,xIni,y,x );
         }
         else if(piece.getClass() == new Knight(null).getClass()){
+        	System.out.println("Knight");
         	result = false;
         }
         else{
+        	System.out.print("initial coords has an object Piece that is unidentified");
         	result = false;
         }
         return result;
@@ -130,19 +138,19 @@ public class Chess {
         //Find direction
         if(yIni-y<0 || xIni - x == 0){//up
             //Direction = "up";
-        	result = isObstructed(board,yIni,xIni,y,x,1,0);
+        	result = isNotObstructed(board,yIni,xIni,y,x,1,0);
         	System.out.print("up");
         }
         else if(yIni - y>0 || xIni - x == 0){//down
-        	result = isObstructed(board,yIni,xIni,y,x,-1,0);
+        	result = isNotObstructed(board,yIni,xIni,y,x,-1,0);
         	System.out.print("down");
         }
         else if(xIni-x<0 || yIni - y == 0){//right
-        	result = isObstructed(board,yIni,xIni,y,x,0,1);
+        	result = isNotObstructed(board,yIni,xIni,y,x,0,1);
         	System.out.print("right");
         }
         else if(xIni - x>0 || yIni - y == 0){//left
-        	result = isObstructed(board,yIni,xIni,y,x,0,-1);
+        	result = isNotObstructed(board,yIni,xIni,y,x,0,-1);
         	System.out.print("left");
         }
         else{//Not a valid rook move
@@ -152,6 +160,47 @@ public class Chess {
         return result;
     }
     
+    private static boolean bishopMove(Board board,int yIni,int xIni,int y,int x){
+        boolean result = false;
+
+        if(yIni <0 || yIni >7 || xIni <0 || xIni >7){
+            throw new IndexOutOfBoundsException("The rook starting position is not on the board");
+        }
+        if(y <0 || y >7 || x <0 || x >7){
+            throw new IndexOutOfBoundsException("The rook destination is not on the board");
+        }
+        
+        if(yIni == y && xIni == x){//the piece has not moved
+            return false;
+        }
+        
+       
+        
+        
+        //Find direction
+        if(yIni-y <0 || xIni - x > 0){//up left
+            //Direction = "up";
+        	result = isNotObstructed(board,yIni,xIni,y,x,1,-1);
+        	System.out.print("upL");
+        }
+        else if(yIni - y <0 || xIni - x < 0){//up right
+        	result = isNotObstructed(board,yIni,xIni,y,x,1,1);
+        	System.out.print("upR");
+        }
+        else if( yIni - y > 0 || xIni-x > 0){//down left
+        	result = isNotObstructed(board,yIni,xIni,y,x,-1,-1);
+        	System.out.print("downL");
+        }
+        else if(yIni - y > 0 || xIni - x < 0){//down right
+        	result = isNotObstructed(board,yIni,xIni,y,x,-1,1);
+        	System.out.print("downR");
+        }
+        else{//Not a valid rook move
+            result = false;
+        }
+   
+        return result;
+    }
     private static boolean validCapture(Board board,int yIni,int xIni,int y,int x ) {
     	//if there is no pieces at the destination than return true
     	//The piece at the destination need to be different than the piece traveling there
@@ -167,21 +216,20 @@ public class Chess {
     	
     }
     
-    private static boolean isObstructed(Board board, int yIni, int xIni, int y, int x, int yStep,int xStep){
-        
+    private static boolean isNotObstructed(Board board, int yIni, int xIni, int y, int x, int yStep,int xStep){
+        //      the next step is y x                   make sure it in range
     	while(!(yIni == y-yStep && xIni == x-xStep) && (yIni < 8 && yIni >= 0 && xIni < 8 && xIni >= 0)) {
     		xIni += xStep;
     		yIni += yStep;
     		
+    		//if there is a piece is the way this will return false
     		if(board.getPiece(yIni, xIni) != null) {
     			return false;
     		}
-    		
-    		
-    		
-    		
+    			
     	}
     	
+    	//
     	if (yIni == y-yStep && xIni == x-xStep){
     		return true;
     	}
@@ -193,7 +241,7 @@ public class Chess {
      * @param args
      */
     public static void main(String[] args) {
-    	System.out.println("test");
+    	
         Board test = new Board("Rookwhite Knightwhite Bishopwhite Queenwhite Kingwhite Bishopwhite Knightwhite Rookwhite \r\n"
         		+ "Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite \r\n"
         		+ "null null null null null null null null \r\n"
@@ -203,10 +251,11 @@ public class Chess {
         		+ "Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack \r\n"
         		+ "Rookblack Knightblack Bishopblack Queenblack Kingblack Bishopblack Knightblack Rookblack ");
 
+        System.out.print("test");//False
         System.out.println(validMove(test, 0, 0, 2, 0));
         
 
-        System.out.println("test1");
+        
         Board test1 = new Board("Rookwhite Knightwhite Bishopwhite Queenwhite Kingwhite Bishopwhite Knightwhite Rookwhite \r\n"
         		+ "null Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite \r\n"
         		+ "null null null null null null null null \r\n"
@@ -216,7 +265,53 @@ public class Chess {
         		+ "Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack \r\n"
         		+ "null Knightblack Bishopblack Queenblack Kingblack Bishopblack Knightblack Rookblack ");
 
-        System.out.println(validMove(test1, 0, 0, 4, 0));
+        System.out.println("test1");//true
+        System.out.println(validMove(test1, 0, 0, 4, 0)+"\r\n");
+        
+        
+        Board test2 = new Board("Rookwhite Knightwhite Bishopwhite Queenwhite Kingwhite Bishopwhite Knightwhite Rookwhite \r\n"
+        		+ "pawnwhite null Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite \r\n"
+        		+ "null null null null null null null null \r\n"
+        		+ "null null null null null null null null \r\n"
+        		+ "null null null null null null null null \r\n"
+        		+ "Rookblack null null null null null null null \r\n"
+        		+ "Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack \r\n"
+        		+ "null Knightblack Bishopblack Queenblack Kingblack Bishopblack Knightblack Rookblack ");
+
+        System.out.println("test2");//true
+        System.out.println(validMove(test2, 0, 2, 2, 0)+"\r\n");
+        
+        
+        
+        Board test3 = new Board("Rookwhite Knightwhite Bishopwhite Queenwhite Kingwhite Bishopwhite Knightwhite Rookwhite \r\n"
+        		+ "pawnwhite null Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite \r\n"
+        		+ "pawnblack null null null null null null null \r\n"
+        		+ "null null null null null null null null \r\n"
+        		+ "null null null null null null null null \r\n"
+        		+ "Rookblack null null null null null null null \r\n"
+        		+ "Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack \r\n"
+        		+ "null Knightblack Bishopblack Queenblack Kingblack Bishopblack Knightblack Rookblack ");
+
+        System.out.println("test3");//True
+        System.out.println(validMove(test3, 0, 2, 2, 0)+"\r\n");
+        //System.out.println(validCapture(test3,0,3,2,0 ));
+        
+        
+        Board test4 = new Board("Rookwhite Knightwhite Bishopwhite Queenwhite Kingwhite Bishopwhite Knightwhite Rookwhite \r\n"
+        		+ "Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite Pawnwhite \r\n"
+        		+ "pawnblack null null null null null null null \r\n"
+        		+ "null null null null null null null null \r\n"
+        		+ "null null null null null null null null \r\n"
+        		+ "Rookblack null null null null null null null \r\n"
+        		+ "Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack Pawnblack \r\n"
+        		+ "null Knightblack Bishopblack Queenblack Kingblack Bishopblack Knightblack Rookblack ");
+
+        System.out.println("test4");//False
+        System.out.println(validMove(test4, 0, 2, 2, 0)+"\r\n");
+        
+        
+        
+        
 
     }
 }
